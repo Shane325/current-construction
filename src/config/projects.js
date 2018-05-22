ba293985f9bd4a66041b1dfb4080fd669489175a
Shane325 <Shane325@gmail.com>
2018-05-22 16:33:27 -0700

    Added more projects and project images.

--- src/config/projects.js
+++ src/config/projects.js
@@ -26,7 +26,7 @@ module.exports = {
             subtext: 'Fremont, California',
             categories: 'project-structural',
             portfolioImg: {
-                src: '../images/projects/project-one_1-320x240.jpg',
+                src: '../images/projects/project-one_1.jpg',
                 alt: 'Fremont project img'
             },
             duration: '4 weeks',
@@ -37,12 +37,28 @@ module.exports = {
             },
             info: 'Installation of two simpson moment frames at two large garage openings. New concrete footings.',
             img: [{
-                    src: '../images/projects/project-one_1-320x240.jpg',
+                    src: '../images/projects/project-one_1.jpg',
                     alt: 'Fremont project img 1'
                 },
                 {
-                    src: '../images/projects/project-one_2-320x240.jpg',
+                    src: '../images/projects/project-one_2.jpg',
                     alt: 'Fremont project img 2'
+                },
+                {
+                    src: '../images/projects/project-one_3.jpg',
+                    alt: 'Fremont project img 3'
+                },
+                {
+                    src: '../images/projects/project-one_4.jpg',
+                    alt: 'Fremont project img 4'
+                },
+                {
+                    src: '../images/projects/project-one_5.jpg',
+                    alt: 'Fremont project img 5'
+                },
+                {
+                    src: '../images/projects/project-one_6.jpg',
+                    alt: 'Fremont project img 6'
                 }
             ]
         },
@@ -52,7 +68,7 @@ module.exports = {
             subtext: 'San Francisco, California',
             categories: 'project-structural',
             portfolioImg: {
-                src: 'http://via.placeholder.com/320x240',
+                src: '../images/projects/project-two_1.jpg',
                 alt: 'Wash img'
             },
             duration: '9 weeks',
@@ -63,12 +79,20 @@ module.exports = {
             },
             info: 'Soft story structural upgrade of a multi unit building. Two Simpson moment frames, five grade brand and shear wall throughout.',
             img: [{
-                    src: 'http://via.placeholder.com/1140x755',
+                    src: '../images/projects/project-two_1.jpg',
                     alt: 'Wash img 1'
                 },
                 {
-                    src: 'http://via.placeholder.com/1140x755',
+                    src: '../images/projects/project-two_2.jpg',
                     alt: 'Wash img 2'
+                },
+                {
+                    src: '../images/projects/project-two_3.jpg',
+                    alt: 'Wash img 3'
+                },
+                {
+                    src: '../images/projects/project-two_4.jpg',
+                    alt: 'Wash img 4'
                 }
             ]
         },
@@ -78,7 +102,7 @@ module.exports = {
             subtext: 'San Francisco, California',
             categories: 'project-structural',
             portfolioImg: {
-                src: 'http://via.placeholder.com/320x240',
+                src: '../images/projects/project-three_1.jpg',
                 alt: 'Bay img'
             },
             duration: '7 weeks',
@@ -89,12 +113,138 @@ module.exports = {
             },
             info: 'Structural upgrade of multi unit building. Reinforced existing steel frame while adding new moment frame to the front with shear walls throughout.',
             img: [{
-                    src: 'http://via.placeholder.com/1140x755',
+                    src: '../images/projects/project-three_1.jpg',
                     alt: 'Bay img 1'
                 },
                 {
-                    src: 'http://via.placeholder.com/1140x755',
+                    src: '../images/projects/project-three_2.jpg',
                     alt: 'Bay img 2'
+                },
+                {
+                    src: '../images/projects/project-three_3.jpg',
+                    alt: 'Bay img 3'
+                }
+            ]
+        },
+        {
+            id: 4,
+            name: 'Bridgeview Drive',
+            subtext: 'San Francisco, California',
+            categories: 'project-residential',
+            portfolioImg: {
+                src: '../images/projects/project-four_1.jpg',
+                alt: 'Bridgeview img'
+            },
+            duration: '4 weeks',
+            type: 'Residential',
+            architectOrEngineer: {
+                name: '',
+                url: ''
+            },
+            info: '',
+            img: [{
+                    src: '../images/projects/project-four_1.jpg',
+                    alt: 'Bridgeview img 1'
+                },
+                {
+                    src: '../images/projects/project-four_2.jpg',
+                    alt: 'Bridgeview img 2'
+                },
+                {
+                    src: '../images/projects/project-four_3.jpg',
+                    alt: 'Bridgeview img 3'
+                },
+                {
+                    src: '../images/projects/project-four_4.jpg',
+                    alt: 'Bridgeview img 4'
+                },
+                {
+                    src: '../images/projects/project-four_5.jpg',
+                    alt: 'Bridgeview img 5'
+                },
+                {
+                    src: '../images/projects/project-four_6.jpg',
+                    alt: 'Bridgeview img 6'
+                },
+                {
+                    src: '../images/projects/project-four_7.jpg',
+                    alt: 'Bridgeview img 7'
+                },
+                {
+                    src: '../images/projects/project-four_8.jpg',
+                    alt: 'Bridgeview img 8'
+                }
+            ]
+        },
+        {
+            id: 5,
+            name: 'Daly City',
+            subtext: 'Daly City, California',
+            categories: 'project-residential',
+            portfolioImg: {
+                src: '../images/projects/project-five_1.jpg',
+                alt: 'Daly city img'
+            },
+            duration: '4 weeks',
+            type: 'Residential',
+            architectOrEngineer: {
+                name: '',
+                url: ''
+            },
+            info: '',
+            img: [{
+                    src: '../images/projects/project-five_1.jpg',
+                    alt: 'Daly city img 1'
+                },
+                {
+                    src: '../images/projects/project-five_2.jpg',
+                    alt: 'Daly city img 2'
+                },
+                {
+                    src: '../images/projects/project-five_3.jpg',
+                    alt: 'Daly city img 3'
+                }
+            ]
+        },
+        {
+            id: 6,
+            name: 'Fillmore Street',
+            subtext: 'San Francisco, California',
+            categories: 'project-residential project-structural',
+            portfolioImg: {
+                src: '../images/projects/project-six_4.jpg',
+                alt: 'Fillmore img'
+            },
+            duration: '9 weeks',
+            type: 'Residential, Structural',
+            architectOrEngineer: {
+                name: '',
+                url: ''
+            },
+            info: '',
+            img: [{
+                    src: '../images/projects/project-six_1.jpg',
+                    alt: 'Fillmore img 1'
+                },
+                {
+                    src: '../images/projects/project-six_2.jpg',
+                    alt: 'Fillmore img 2'
+                },
+                {
+                    src: '../images/projects/project-six_3.jpg',
+                    alt: 'Fillmore img 3'
+                },
+                {
+                    src: '../images/projects/project-six_4.jpg',
+                    alt: 'Fillmore img 4'
+                },
+                {
+                    src: '../images/projects/project-six_5.jpg',
+                    alt: 'Fillmore img 5'
+                },
+                {
+                    src: '../images/projects/project-six_6.jpg',
+                    alt: 'Fillmore img 6'
                 }
             ]
         }
