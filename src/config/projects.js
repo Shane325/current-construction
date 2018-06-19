03e6fa6ee5cb7e6385f1cd1fe73a0ec023c68d42
Shane325 <Shane325@gmail.com>
2018-06-19 12:40:55 -0700

    Updated projects menu and some project photos.

--- src/config/projects.js
+++ src/config/projects.js
@@ -1,252 +1,402 @@
-'use strict';
+'use strict'
 
 /*
  * Module dependencies
  */
 
 module.exports = {
-    pageTitle: 'Projects',
-    state: 'projects',
-    title: {
-        text: 'Projects',
-        subtext: 'See our work',
-        menu: [{
-                name: 'Home',
-                url: '/'
-            },
-            {
-                name: 'Projects',
-                url: null
-            }
-        ]
+  pageTitle: 'Projects',
+  state: 'projects',
+  title: {
+    text: 'Projects',
+    subtext: 'See our work',
+    menu: [{
+      name: 'Home',
+      url: '/'
     },
-    projects: [{
-            id: 1,
-            name: 'Fremont Project',
-            subtext: 'Fremont, California',
-            categories: 'project-structural',
-            portfolioImg: {
-                src: '../images/projects/project-one_1.jpg',
-                alt: 'Fremont project img'
-            },
-            duration: '4 weeks',
-            type: 'Structural',
-            architectOrEngineer: {
-                name: 'Spartac Bet-Lachin Structural Engineer',
-                url: 'http://spartacbetlachin.com/'
-            },
-            info: 'Installation of two simpson moment frames at two large garage openings. New concrete footings.',
-            img: [{
-                    src: '../images/projects/project-one_1.jpg',
-                    alt: 'Fremont project img 1'
-                },
-                {
-                    src: '../images/projects/project-one_2.jpg',
-                    alt: 'Fremont project img 2'
-                },
-                {
-                    src: '../images/projects/project-one_3.jpg',
-                    alt: 'Fremont project img 3'
-                },
-                {
-                    src: '../images/projects/project-one_4.jpg',
-                    alt: 'Fremont project img 4'
-                },
-                {
-                    src: '../images/projects/project-one_5.jpg',
-                    alt: 'Fremont project img 5'
-                },
-                {
-                    src: '../images/projects/project-one_6.jpg',
-                    alt: 'Fremont project img 6'
-                }
-            ]
-        },
-        {
-            id: 2,
-            name: 'Washington Street',
-            subtext: 'San Francisco, California',
-            categories: 'project-structural',
-            portfolioImg: {
-                src: '../images/projects/project-two_1.jpg',
-                alt: 'Wash img'
-            },
-            duration: '9 weeks',
-            type: 'Structural',
-            architectOrEngineer: {
-                name: 'Coffman Engineers',
-                url: 'https://www.coffman.com/'
-            },
-            info: 'Soft story structural upgrade of a multi unit building. Two Simpson moment frames, five grade brand and shear wall throughout.',
-            img: [{
-                    src: '../images/projects/project-two_1.jpg',
-                    alt: 'Wash img 1'
-                },
-                {
-                    src: '../images/projects/project-two_2.jpg',
-                    alt: 'Wash img 2'
-                },
-                {
-                    src: '../images/projects/project-two_3.jpg',
-                    alt: 'Wash img 3'
-                },
-                {
-                    src: '../images/projects/project-two_4.jpg',
-                    alt: 'Wash img 4'
-                }
-            ]
-        },
-        {
-            id: 3,
-            name: 'Bay Street',
-            subtext: 'San Francisco, California',
-            categories: 'project-structural',
-            portfolioImg: {
-                src: '../images/projects/project-three_1.jpg',
-                alt: 'Bay img'
-            },
-            duration: '7 weeks',
-            type: 'Structural',
-            architectOrEngineer: {
-                name: 'Double D Engineering',
-                url: 'http://www.doubledengineering.com'
-            },
-            info: 'Structural upgrade of multi unit building. Reinforced existing steel frame while adding new moment frame to the front with shear walls throughout.',
-            img: [{
-                    src: '../images/projects/project-three_1.jpg',
-                    alt: 'Bay img 1'
-                },
-                {
-                    src: '../images/projects/project-three_2.jpg',
-                    alt: 'Bay img 2'
-                },
-                {
-                    src: '../images/projects/project-three_3.jpg',
-                    alt: 'Bay img 3'
-                }
-            ]
-        },
-        {
-            id: 4,
-            name: 'Bridgeview Drive',
-            subtext: 'San Francisco, California',
-            categories: 'project-residential',
-            portfolioImg: {
-                src: '../images/projects/project-four_1.jpg',
-                alt: 'Bridgeview img'
-            },
-            duration: '4 weeks',
-            type: 'Residential',
-            architectOrEngineer: {
-                name: '',
-                url: ''
-            },
-            info: '',
-            img: [{
-                    src: '../images/projects/project-four_1.jpg',
-                    alt: 'Bridgeview img 1'
-                },
-                {
-                    src: '../images/projects/project-four_2.jpg',
-                    alt: 'Bridgeview img 2'
-                },
-                {
-                    src: '../images/projects/project-four_3.jpg',
-                    alt: 'Bridgeview img 3'
-                },
-                {
-                    src: '../images/projects/project-four_4.jpg',
-                    alt: 'Bridgeview img 4'
-                },
-                {
-                    src: '../images/projects/project-four_5.jpg',
-                    alt: 'Bridgeview img 5'
-                },
-                {
-                    src: '../images/projects/project-four_6.jpg',
-                    alt: 'Bridgeview img 6'
-                },
-                {
-                    src: '../images/projects/project-four_7.jpg',
-                    alt: 'Bridgeview img 7'
-                },
-                {
-                    src: '../images/projects/project-four_8.jpg',
-                    alt: 'Bridgeview img 8'
-                }
-            ]
-        },
-        {
-            id: 5,
-            name: 'Daly City',
-            subtext: 'Daly City, California',
-            categories: 'project-residential',
-            portfolioImg: {
-                src: '../images/projects/project-five_1.jpg',
-                alt: 'Daly city img'
-            },
-            duration: '4 weeks',
-            type: 'Residential',
-            architectOrEngineer: {
-                name: '',
-                url: ''
-            },
-            info: '',
-            img: [{
-                    src: '../images/projects/project-five_1.jpg',
-                    alt: 'Daly city img 1'
-                },
-                {
-                    src: '../images/projects/project-five_2.jpg',
-                    alt: 'Daly city img 2'
-                },
-                {
-                    src: '../images/projects/project-five_3.jpg',
-                    alt: 'Daly city img 3'
-                }
-            ]
-        },
-        {
-            id: 6,
-            name: 'Fillmore Street',
-            subtext: 'San Francisco, California',
-            categories: 'project-residential project-structural',
-            portfolioImg: {
-                src: '../images/projects/project-six_4.jpg',
-                alt: 'Fillmore img'
-            },
-            duration: '9 weeks',
-            type: 'Residential, Structural',
-            architectOrEngineer: {
-                name: '',
-                url: ''
-            },
-            info: '',
-            img: [{
-                    src: '../images/projects/project-six_1.jpg',
-                    alt: 'Fillmore img 1'
-                },
-                {
-                    src: '../images/projects/project-six_2.jpg',
-                    alt: 'Fillmore img 2'
-                },
-                {
-                    src: '../images/projects/project-six_3.jpg',
-                    alt: 'Fillmore img 3'
-                },
-                {
-                    src: '../images/projects/project-six_4.jpg',
-                    alt: 'Fillmore img 4'
-                },
-                {
-                    src: '../images/projects/project-six_5.jpg',
-                    alt: 'Fillmore img 5'
-                },
-                {
-                    src: '../images/projects/project-six_6.jpg',
-                    alt: 'Fillmore img 6'
-                }
-            ]
-        }
+    {
+      name: 'Projects',
+      url: null
+    }
     ]
-};
+  },
+  projects: [{
+    id: 1,
+    name: 'Fremont Project',
+    subtext: 'Fremont, California',
+    categories: 'project-structural',
+    portfolioImg: {
+      src: '../images/projects/project-one_1.jpg',
+      alt: 'Fremont project img'
+    },
+    duration: '4 weeks',
+    type: 'Structural',
+    architectOrEngineer: {
+      name: 'Spartac Bet-Lachin Structural Engineer',
+      url: 'http://spartacbetlachin.com/'
+    },
+    info: 'Installation of two simpson moment frames at two large garage openings. New concrete footings.',
+    img: [{
+      src: '../images/projects/project-one_1.jpg',
+      alt: 'Fremont project img 1'
+    },
+    {
+      src: '../images/projects/project-one_2.jpg',
+      alt: 'Fremont project img 2'
+    },
+    {
+      src: '../images/projects/project-one_3.jpg',
+      alt: 'Fremont project img 3'
+    },
+    {
+      src: '../images/projects/project-one_4.jpg',
+      alt: 'Fremont project img 4'
+    },
+    {
+      src: '../images/projects/project-one_5.jpg',
+      alt: 'Fremont project img 5'
+    },
+    {
+      src: '../images/projects/project-one_6.jpg',
+      alt: 'Fremont project img 6'
+    }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Washington Street',
+    subtext: 'San Francisco, California',
+    categories: 'project-structural',
+    portfolioImg: {
+      src: '../images/projects/project-two_1.jpg',
+      alt: 'Wash img'
+    },
+    duration: '9 weeks',
+    type: 'Structural',
+    architectOrEngineer: {
+      name: 'Coffman Engineers',
+      url: 'https://www.coffman.com/'
+    },
+    info: 'Soft story structural upgrade of a multi unit building. Two Simpson moment frames, five grade brand and shear wall throughout.',
+    img: [{
+      src: '../images/projects/project-two_1.jpg',
+      alt: 'Wash img 1'
+    },
+    {
+      src: '../images/projects/project-two_2.jpg',
+      alt: 'Wash img 2'
+    },
+    {
+      src: '../images/projects/project-two_3.jpg',
+      alt: 'Wash img 3'
+    },
+    {
+      src: '../images/projects/project-two_4.jpg',
+      alt: 'Wash img 4'
+    }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Bay Street',
+    subtext: 'San Francisco, California',
+    categories: 'project-structural',
+    portfolioImg: {
+      src: '../images/projects/project-three_1.jpg',
+      alt: 'Bay img'
+    },
+    duration: '7 weeks',
+    type: 'Structural',
+    architectOrEngineer: {
+      name: 'Double D Engineering',
+      url: 'http://www.doubledengineering.com'
+    },
+    info: 'Structural upgrade of multi unit building. Reinforced existing steel frame while adding new moment frame to the front with shear walls throughout.',
+    img: [{
+      src: '../images/projects/project-three_1.jpg',
+      alt: 'Bay img 1'
+    },
+    {
+      src: '../images/projects/project-three_2.jpg',
+      alt: 'Bay img 2'
+    },
+    {
+      src: '../images/projects/project-three_3.jpg',
+      alt: 'Bay img 3'
+    }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Bridgeview Drive',
+    subtext: 'San Francisco, California',
+    categories: 'project-residential',
+    portfolioImg: {
+      src: '../images/projects/project-four_1.jpg',
+      alt: 'Bridgeview img'
+    },
+    duration: '4 weeks',
+    type: 'Residential',
+    architectOrEngineer: {
+      name: '',
+      url: ''
+    },
+    info: '',
+    img: [{
+      src: '../images/projects/project-four_1.jpg',
+      alt: 'Bridgeview img 1'
+    },
+    {
+      src: '../images/projects/project-four_2.jpg',
+      alt: 'Bridgeview img 2'
+    },
+    {
+      src: '../images/projects/project-four_3.jpg',
+      alt: 'Bridgeview img 3'
+    },
+    {
+      src: '../images/projects/project-four_4.jpg',
+      alt: 'Bridgeview img 4'
+    },
+    {
+      src: '../images/projects/project-four_5.jpg',
+      alt: 'Bridgeview img 5'
+    },
+    {
+      src: '../images/projects/project-four_6.jpg',
+      alt: 'Bridgeview img 6'
+    },
+    {
+      src: '../images/projects/project-four_7.jpg',
+      alt: 'Bridgeview img 7'
+    },
+    {
+      src: '../images/projects/project-four_8.jpg',
+      alt: 'Bridgeview img 8'
+    }
+    ]
+  },
+  {
+    id: 5,
+    name: 'Daly City',
+    subtext: 'Daly City, California',
+    categories: 'project-residential',
+    portfolioImg: {
+      src: '../images/projects/project-five_1.jpg',
+      alt: 'Daly city img'
+    },
+    duration: '4 weeks',
+    type: 'Residential',
+    architectOrEngineer: {
+      name: '',
+      url: ''
+    },
+    info: '',
+    img: [{
+      src: '../images/projects/project-five_1.jpg',
+      alt: 'Daly city img 1'
+    },
+    {
+      src: '../images/projects/project-five_2.jpg',
+      alt: 'Daly city img 2'
+    },
+    {
+      src: '../images/projects/project-five_3.jpg',
+      alt: 'Daly city img 3'
+    }
+    ]
+  },
+  {
+    id: 6,
+    name: 'Fillmore Street',
+    subtext: 'San Francisco, California',
+    categories: 'project-residential',
+    portfolioImg: {
+      src: '../images/projects/project-six_4.jpg',
+      alt: 'Fillmore img'
+    },
+    duration: '9 weeks',
+    type: 'Residential, Structural',
+    architectOrEngineer: {
+      name: '',
+      url: ''
+    },
+    info: '',
+    img: [{
+      src: '../images/projects/project-six_1.jpg',
+      alt: 'Fillmore img 1'
+    },
+    {
+      src: '../images/projects/project-six_2.jpg',
+      alt: 'Fillmore img 2'
+    },
+    {
+      src: '../images/projects/project-six_3.jpg',
+      alt: 'Fillmore img 3'
+    },
+    {
+      src: '../images/projects/project-six_4.jpg',
+      alt: 'Fillmore img 4'
+    },
+    {
+      src: '../images/projects/project-six_5.jpg',
+      alt: 'Fillmore img 5'
+    },
+    {
+      src: '../images/projects/project-six_6.jpg',
+      alt: 'Fillmore img 6'
+    }
+    ]
+  },
+  {
+    id: 7,
+    name: '7th Street',
+    subtext: 'San Francisco, California',
+    categories: 'project-commercial',
+    portfolioImg: {
+      src: '../images/projects/project-seven_3.jpg',
+      alt: '7th img'
+    },
+    duration: '',
+    type: 'Commercial',
+    architectOrEngineer: {
+      name: '',
+      url: ''
+    },
+    info: 'We transformed this old film studio into a modern office space for a tech start-up.',
+    img: [{
+      src: '../images/projects/project-seven_1.jpg',
+      alt: '7th img 1'
+    },
+    {
+      src: '../images/projects/project-seven_2.jpg',
+      alt: '7th img 2'
+    },
+    {
+      src: '../images/projects/project-seven_3.jpg',
+      alt: '7th img 3'
+    }
+    ]
+  },
+  {
+    id: 8,
+    name: '2630 Mission Street',
+    subtext: 'San Francisco, California',
+    categories: 'project-commercial',
+    portfolioImg: {
+      src: 'http://via.placeholder.com/300x300',
+      alt: 'Mission street img'
+    },
+    duration: '',
+    type: 'Commercial',
+    architectOrEngineer: {
+      name: '',
+      url: ''
+    },
+    info: '',
+    img: [{
+      src: 'http://via.placeholder.com/300x300',
+      alt: 'Mission img 1'
+    },
+    {
+      src: 'http://via.placeholder.com/300x300',
+      alt: 'Mission img 2'
+    },
+    {
+      src: 'http://via.placeholder.com/300x300',
+      alt: 'Mission img 3'
+    }
+    ]
+  },
+  {
+    id: 9,
+    name: '1035 Market Street',
+    subtext: 'San Francisco, California',
+    categories: 'project-commercial',
+    portfolioImg: {
+      src: 'http://via.placeholder.com/300x300',
+      alt: 'Market street img'
+    },
+    duration: '',
+    type: 'Commercial',
+    architectOrEngineer: {
+      name: '',
+      url: ''
+    },
+    info: '',
+    img: [{
+      src: 'http://via.placeholder.com/300x300',
+      alt: 'Market img 1'
+    },
+    {
+      src: 'http://via.placeholder.com/300x300',
+      alt: 'Market img 2'
+    },
+    {
+      src: 'http://via.placeholder.com/300x300',
+      alt: 'Market img 3'
+    }
+    ]
+  },
+  {
+    id: 10,
+    name: '1244 2nd Avenue',
+    subtext: 'Oakland, California',
+    categories: 'project-multi-unit',
+    portfolioImg: {
+      src: 'http://via.placeholder.com/300x300',
+      alt: '2nd ave img'
+    },
+    duration: '',
+    type: 'Multi-Unit',
+    architectOrEngineer: {
+      name: '',
+      url: ''
+    },
+    info: '',
+    img: [{
+      src: 'http://via.placeholder.com/300x300',
+      alt: '2nd ave img 1'
+    },
+    {
+      src: 'http://via.placeholder.com/300x300',
+      alt: '2nd ave img 2'
+    },
+    {
+      src: 'http://via.placeholder.com/300x300',
+      alt: '2nd ave img 3'
+    }
+    ]
+  },
+  {
+    id: 11,
+    name: '1825 Mission Street',
+    subtext: 'San Francisco, California',
+    categories: 'project-multi-unit',
+    portfolioImg: {
+      src: 'http://via.placeholder.com/300x300',
+      alt: '1825 mission img'
+    },
+    duration: '',
+    type: 'Multi-Unit',
+    architectOrEngineer: {
+      name: '',
+      url: ''
+    },
+    info: '',
+    img: [{
+      src: 'http://via.placeholder.com/300x300',
+      alt: '1825 mission img 1'
+    },
+    {
+      src: 'http://via.placeholder.com/300x300',
+      alt: '1825 mission img 2'
+    },
+    {
+      src: 'http://via.placeholder.com/300x300',
+      alt: '1825 mission img 3'
+    }
+    ]
+  }
+  ]
+}
